2358089735e25f32f04179ed9b1b1a3d59c501ff
AntonPopov90 <mikprom12@>
2023-09-08 14:31:34 +0300

    add autofill fuel data

--- write_to_excel.py
+++ write_to_excel.py
@@ -1,12 +1,24 @@
 import openpyxl
 import calendar
-from check_file import cheque_verify 
+from check_file import cheque_verify
 
 
 excel_file = 'gsm.xlsx'
 check_file = 'check.jpg'
 wb = openpyxl.load_workbook(excel_file)
 sheet = wb.active
+
+
+def calculate_distance(cheque_data: dict) -> int:
+    if type(cheque_data) is not dict:
+        return 'error'
+    else:
+        sum = int(cheque_data['sum'])
+        distance = int((sum/45.9)/0.11)
+        return distance
+
+
+print(calculate_distance(cheque_verify(check_file)))
 
 
 def fill_dates(cheque_data: dict) -> None:
@@ -18,19 +30,31 @@ def fill_dates(cheque_data: dict) -> None:
         month = cheque_data['date'].month  # get month from cheque
 
         k = 0
-        date_cell = 10  # cell of first date
+        first_cell = 10  # cell with first date       
         for day in range(1, 32):
             try:
                 weekday = calendar.weekday(year, month, day)
             except ValueError:
                 continue
             if weekday < calendar.SATURDAY:
-                date_cell += 1
-                x = f'A{date_cell}'
+                first_cell += 1
+                date = f'A{first_cell}'
+                distance_cell = f'D{first_cell}'
+                fuel_consumption = f'E{first_cell}'
+                fuel_price = f'F{first_cell}'
                 k += 1
-        # format the result
-                sheet[x] = "%02d.%02d.%d" % (day, month, year)  # fill dates
+                sheet[date] = "%02d.%02d.%d" % (day, month, year)
+                sheet[fuel_consumption] = 0.11
+                sheet[fuel_price] = 45.9
+                wb.save('gsm.xlsx')  # fill dates
+
+        everyday_distance = int(calculate_distance(cheque_verify(check_file))/k)
+        for i in range(11, k+11):
+            distance_cell = f'D{i}'
+            sheet[distance_cell] = everyday_distance
+        
         wb.save('gsm.xlsx')
+        
         return 'all right'
 
 
